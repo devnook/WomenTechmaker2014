d70e08017fc0a4f693b8085b081f34bfcc91c504
Ewa Gasperowicz <devnook@gmail.com>
2013-11-08 13:16:00 +0100

    First pages commit

---
+++ Knowledge-Based Application Design Patterns - Google IO 2012_files/slide_config.js
@@ -0,0 +1,34 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Knowledge-Based<br>Application Design Patterns',
+    subtitle: '',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1',
+    favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Inconsolata'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Shawn Simister',
+    company: 'Developer Programs Engineer, Google',
+    gplus: 'http://plus.google.com/109936836907132434202',
+    twitter: '@narphorium'
+  }/*, {
+    name: 'Eric Bidelman',
+    gplus: 'http://plus.ericbidelman.com',
+    company: 'Senior Developer Programs Engineer, Google Chrome',
+    twitter: '@ebidel',
+    www: 'http://www.ericbidelman.com',
+    github: 'http://github.com/ebidel'
+  }*/]
+};
+
